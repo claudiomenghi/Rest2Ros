1fd99562692199a63f727643daec3e3eae4db628
claudiomenghi <cla.menghi@gmail.com>
2017-10-12 09:47:38 +0200

    update the mission to be sent

--- src/main/java/se/got/Co4robotsGUI.java
+++ src/main/java/se/got/Co4robotsGUI.java
@@ -387,7 +387,7 @@ public class Co4robotsGUI extends javax.swing.JFrame implements PSPController {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                MissionSender sender=new MissionSender();
                try {
-				sender.send("AAA");
+				sender.send("<> (r1 && <> r2 && <> r3)");
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
