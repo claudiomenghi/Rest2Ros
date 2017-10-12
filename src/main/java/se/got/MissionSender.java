8bb327fe0200078ef2880cdfc093c1ee86f69f0a
claudiomenghi <cla.menghi@gmail.com>
2017-10-12 10:43:17 +0200

    updates the receiver

--- src/main/java/se/got/MissionSender.java
+++ src/main/java/se/got/MissionSender.java
@@ -24,7 +24,9 @@ public class MissionSender {
 		HttpPost httppost = new HttpPost("http://127.0.0.1:13000");
 
 		List<NameValuePair> params = new ArrayList<NameValuePair>(1);
-		params.add(new BasicNameValuePair("mission", URLEncoder.encode(mission, "UTF-8")));
+		String sentMission=URLEncoder.encode(mission, "UTF-8");
+		System.out.println("mission send: "+sentMission);
+		params.add(new BasicNameValuePair("mission",sentMission ));
 
 		httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
 
