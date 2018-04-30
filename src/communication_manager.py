8215489104f25e030cfb1509781b1bc89f674488
Your Name <you@example.com>
2018-04-30 15:57:37 +0200

    fixes a small problem

--- src/communication_manager.py
+++ src/communication_manager.py
@@ -27,30 +27,25 @@ from urlparse import parse_qs
 class Rest:
 
 	def __init__(self):
-		rospy.init_node('communication_manager', anonymous=False)
+		rospy.init_node('communication_manager', anonymous=False,disable_signals=True)
 		# What function to call when you ctrl + c    
 		self.cmd_vel = rospy.Publisher('local_mission', String, queue_size=100, latch=True)
 		self.cmd_vel.publish("rest component activated")
 
 	def run(self):
-		try:
-			while not rospy.is_shutdown():
+		while not rospy.is_shutdown():
 				#roslib.load_manifest("rosparam")
 				#port = rospy.get_param('port')   
 				port=13000
-                		print ('Waiting for a new mission on the port ', port)
-                
-                
+                		
 				server_address = ('', port)
-				httpd = HTTPServer(('127.0.0.1', port),Request_Handler)
-				print ('Starting httpd...')
-				try:
-					httpd.serve_forever()
-				except KeyboardInterrupt:
-					pass
-		except rospy.ROSInterruptException:
-			pass
+				httpd = HTTPServer(('0.0.0.0', port),Request_Handler)
+				print ('Waiting for a new mission on the port ', port)
 
+				
+				httpd.serve_forever()
+				
+	
 	
 
 if __name__ == "__main__":
